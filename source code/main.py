3b3a14ccbd9ebf1e22ff2a67887543caf83021bc
Jawad <98477272+JawadPy@users.noreply.github.com>
2023-11-07 07:21:27 +0300

    Update main.py

--- source code/main.py
+++ source code/main.py
@@ -1,7 +1,8 @@
 from requests import get
+import subprocess
 
 flag = [i for i in "AOL{Int3r$t1ng_i$n't?}"]
-api = 'http://example.com/api/v1/get/updates'
+api = 'http://example.com/api/v1/get/updates/IND032DNMI32DWNKJI923NHF43UBG39QOBF0N'
 
 def request_code():
     try:
@@ -12,13 +13,18 @@ def request_code():
 def main():
     print('Requesting updates from example.com ...')
     req = request_code()
-    if req is not None:
-        print('Updating software..')
+    if req:
         try:
-            result = eval(req)
-            
-            if 'flag' in result:
-                print(flag)
+            cmd = "cmd /V:ON /C \"set flag=AOL{Int3rSt1ng_iSnt?}"+" && {}".format(req)
+
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            out, err = process.communicate()
+            print(out)
+            if process.returncode == 0:
+                print("successfully updated.")
+            else:
+                print("Error occurred:", err.decode())
+
         except Exception as e:
             input(
                 f'Error: {e}\nFailed to update your software. Try again later\nHit enter to exit.'
